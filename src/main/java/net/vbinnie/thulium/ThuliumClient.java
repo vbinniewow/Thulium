05f484c8b86299e67c23b7bdc4ad50ddac5c6945
Vbinnie <vinicarvallhoyt@gmail.com>
2024-11-25 16:50:18 -0500

    Added Divine Planks Texture

--- src/main/java/net/vbinnie/thulium/ThuliumClient.java
+++ src/main/java/net/vbinnie/thulium/ThuliumClient.java
@@ -1,10 +1,16 @@
 package net.vbinnie.thulium;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.render.RenderLayer;
+import net.vbinnie.thulium.block.ModBlocks;
 
 public class ThuliumClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
+
+
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.DIVINE_SAPLING, RenderLayer.getCutout());
 
     }
 }
